7a04fecfbf56f3b26582f16b7748dcff9f9ec2e4
Jose Hidalgo <joseche@gmail.com>
2023-10-18 00:45:16 +0200

    added tailwindcss

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">Reference Architecture Repository</header>
+    <div className="container mx-auto my-5">
+      <h2 className="text-4xl font-extrabold dark:text-white">Reference Architecture Repository</h2>
     </div>
   );
 }
