4644fd076be604f9fdef795f6ecf8fb1cd436f77
Jose Hidalgo <joseche@gmail.com>
2023-10-18 00:21:41 +0200

    first stab at the frontend

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders reference architecture somewhere', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const textElement = screen.getByText(/reference architecture/i);
+  expect(textElement).toBeInTheDocument();
 });
