f40be6e5b1a54e82b542bc2fa1eb8aa4aa1e15ed
Jose Hidalgo <joseche@gmail.com>
2023-11-01 02:51:34 +0100

    adding a checkbox to countrycard

--- frontend/src/components/CountryCard.js
+++ frontend/src/components/CountryCard.js
@@ -2,22 +2,21 @@ import React from "react";
 
 function CountryCard(country) {
   return (
-    <div className="col-md-6 col-lg-4">
-      <div className="card border-0">
-        <a href="#">
-          <img
-            className="card-img-top scale-on-hover"
-            src={country.img}
-            alt="Card Image"
+    <div className="card mx-2 my-2" style={{ width: "18rem" }}>
+      <img className="card-img-top" src={country.img} alt="Card image cap" />
+      <div className="card-body">
+        <h5 className="card-title">{country.name}</h5>
+        <p className="card-text">{country.desc}</p>
+        <div className="form-check form-switch">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            role="switch"
+            id={country.id}
           />
-        </a>
-        <div className="card-body">
-          <h6>
-            <a href="#">{country.name}</a>
-          </h6>
-          <p className="text-muted card-text">
-            {country.desc}
-          </p>
+          <label className="form-check-label" htmlFor={country.id}>
+            Add to Indicators
+          </label>
         </div>
       </div>
     </div>
--- frontend/src/pages/CountriesPage.js
+++ frontend/src/pages/CountriesPage.js
@@ -1,26 +1,18 @@
 import React from "react";
 import CountryCard from "../components/CountryCard";
 
-const countries = [
-  {
-    name: "Afganistan",
-    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/63/Nomads_in_Badghis_Province.jpg/1920px-Nomads_in_Badghis_Province.jpg",
-    desc: "Afghanistan,[d] officially the Islamic Emirate of Afghanistan,[e] is a landlocked country located at the crossroads of Central Asia and South Asia. Referred to as the Heart of Asia,[26] it is bordered by Pakistan to the east and south,[f] Iran to the west, Turkmenistan to the northwest, Uzbekistan to the north, Tajikistan to the northeast, and China to the northeast and east.",
-  },
-  {
-    name: "Costa Rica",
-    img: "https://upload.wikimedia.org/wikipedia/commons/b/bd/Iglesia_Inmaculada_Concepcion_Ujarras.jpg",
-    desc: "Costa Rica, officially the Republic of Costa Rica (Spanish: República de Costa Rica), is a country in the Central American region of North America. Costa Rica is bordered by Nicaragua to the north, the Caribbean Sea to the northeast, Panama to the southeast, and the Pacific Ocean to the southwest, as well as maritime border with Ecuador to the south of Cocos Island.",
-  },
-];
+import countries from "../fixtures/countries.json"
 
 function CountriesPage() {
   return (
     <div className="container" style={{ paddingTop: "120px" }}>
-      <main className="page landing-page">Countries page</main>
+      <main className="page landing-page">
+        <h1 className="bd-title">Countries</h1>
+        <p className="bd-lead">Select the countries for which you want to see information</p>
+      </main>
       <div className="row">
         {countries.map(country => (
-          <CountryCard key={country.name} name={country.name} img={country.img} desc={country.desc} />
+          <CountryCard key={country.id} name={country.name} img={country.img} desc={country.desc} />
         ))}
       </div>
     </div>
